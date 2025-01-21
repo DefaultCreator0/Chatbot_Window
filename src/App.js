d6b6b1c4be93648ded08d7b0baee586a60802792
DefaultCreator0 <95504244+DefaultCreator0@users.noreply.github.com>
2025-01-21 18:56:20 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+export default function TodoList() {
+  return (
+    <div className="outer">
+      <h3>Test Window</h3>
+
+      <div className="inner">
+        <h3>Inner Box</h3>
+      </div>
+
+      <div className="Seperator"></div>
+
+      <div className="User">
+        <h3>Test text</h3>
+      </div>
+      <div className="Button"></div>
+    </div>
+  );
+}
